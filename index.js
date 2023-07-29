c6f0af1a80e2bc7d3ec10235bffc994921d4ce41
thirukumaran05 <learningthiru@gmail.com>
2023-07-29 10:06:16 +0530

    Complete Riddles Project

---
+++ index.js
@@ -0,0 +1,44 @@
+const url = "https://riddles-api.vercel.app/random";
+const container = document.querySelector(".container");
+const section = document.querySelector(".section");
+const questionSection = document.querySelector(".question");
+const answerSection = document.querySelector(".answer");
+const answerSectionh2 = document.querySelector(".answer h2");
+const show = document.getElementById("answer");
+const next = document.getElementById("next");
+var answerArray = [];
+
+async function serverResponse(){
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result.riddle);
+    console.log(result.answer);
+    answerArray.push({
+        riddle : result.riddle,
+        answer : result.answer
+    })
+    console.log(answerArray[0]);
+
+    questionSection.innerHTML = `<h2>${answerArray[0].riddle}</h2>`
+
+    show.addEventListener("click", function(){
+        container.style.height = "600px";
+        section.style.height = "400px";
+        questionSection.classList.add("remove-top");
+        answerSection.classList.add("roll");
+        answerSectionh2.classList.add("roll");
+        answerSection.innerHTML = `<h2>${answerArray[0].answer}</h2>`
+        answerSection.scrollIntoView(true)
+    })
+
+    next.addEventListener("click", function(){
+        container.style.height = "400px";
+        section.style.height = "240px";
+        questionSection.classList.remove("remove-top");
+        answerSection.classList.remove("roll");
+        answerSectionh2.classList.remove("roll");
+        window.location.reload();
+    })
+}
+
+serverResponse();
